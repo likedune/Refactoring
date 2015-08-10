48e8cdf29cadaa59db4b767121688a7f0e520a14
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-08-10 16:00:21 +0900

    다른 클래스의 속성을 인자로 사용하지 않기 위해 함수를 옮기고  렌탈 클래스에는 그것을 사용하는 코드를 만듦

--- src/ch1/Movie.java
+++ src/ch1/Movie.java
@@ -25,4 +25,37 @@ public class Movie {
     public String getTitle() {
         return title;
     }
+
+    public double getCharge(int daysRented) {
+        double charge = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                charge += 2;
+                if (daysRented > 2) {
+                    charge += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                charge += daysRented * 3;
+                break;
+            case Movie.CHILDRUNS:
+                charge += 1.5;
+                if (daysRented > 3) {
+                    charge += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+
+        return charge;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        // 최신물을 이틀 이상 대여하면 2포인트, 아니면 1포인트를 지급
+        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
+            return 2;
+        }
+        else {
+            return 1;
+        }
+    }
 }
--- src/ch1/Rental.java
+++ src/ch1/Rental.java
@@ -19,35 +19,10 @@ public class Rental {
     }
 
     public double getCharge() {
-        double amount = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (getDaysRented() > 2) {
-                    amount += (getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                amount += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRUNS:
-                amount += 1.5;
-                if (getDaysRented() > 3) {
-                    amount += (getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-
-        return amount;
+        return movie.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints() {
-        // 최신물을 이틀 이상 대여하면 2포인트, 아니면 1포인트를 지급
-        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
-            return 2;
-        }
-        else {
-            return 1;
-        }
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
