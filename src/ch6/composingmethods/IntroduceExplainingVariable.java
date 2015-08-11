d3d23411308ca33433ab7fc9f0f94f17919e3904
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-08-11 14:58:38 +0900

    직관적 임시변수 사용에서 값 계산 기준에 따라 임시변수를 만듦

--- src/ch6/composingmethods/IntroduceExplainingVariable.java
+++ src/ch6/composingmethods/IntroduceExplainingVariable.java
@@ -7,8 +7,9 @@ public class IntroduceExplainingVariable {
 
     public double getPrice() {
         // price = 구매액 - 대량구매 할인 + 배송비
-        return itemPrice * quantity
-                - Math.max(0, quantity - 500) * itemPrice * 0.05
-                + Math.min(quantity * itemPrice * 0.1, 100);
+        final double basePrice = itemPrice * quantity;
+        final double quantityDiscount = Math.max(0, quantity - 500) * itemPrice * 0.05;
+        final double shippingPrice = Math.min(basePrice * 0.1, 100);
+        return basePrice - quantityDiscount + shippingPrice;
     }
 }
