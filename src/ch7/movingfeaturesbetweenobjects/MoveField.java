7f27de6b7ba05635424eb4e8fa5bf22729b4500b
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-08-12 15:31:54 +0900

    필드 이동에서 필드 캡슐화를 통해 원본 객체의 인스턴스를 대상 객체로 이동시킴

--- src/ch7/movingfeaturesbetweenobjects/MoveField.java
+++ src/ch7/movingfeaturesbetweenobjects/MoveField.java
@@ -3,14 +3,22 @@ package ch7.movingfeaturesbetweenobjects;
 public class MoveField {
 
     public class AccountType {
+        private double interestRate;
+
+        public void setInterestRate(double interestRate) {
+            this.interestRate = interestRate;
+        }
+
+        public double getInterestRate() {
+            return interestRate;
+        }
     }
 
     public class Account {
         private AccountType type;
-        private double interestRate;
 
         public double interestForAmountDays(double amount, int days) {
-            return interestRate * amount * days / 365;
+            return type.getInterestRate() * amount * days / 365;
         }
     }
 }
