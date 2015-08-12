420b90f2294b5418379bdbcce29acf63ef770cdb
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-08-12 16:58:32 +0900

    클래스 추출에서 Person 클래스에서 전화 객체를 빼냄

--- src/ch7/movingfeaturesbetweenobjects/ExtractClass.java
+++ src/ch7/movingfeaturesbetweenobjects/ExtractClass.java
@@ -2,29 +2,54 @@ package ch7.movingfeaturesbetweenobjects;
 
 public class ExtractClass {
 
+    public class TelephoneNumber {
+        private String areaCode;
+        private String number;
+
+        public TelephoneNumber(String areaCode, String number) {
+            this.areaCode = areaCode;
+            this.number = number;
+        }
+
+        public void setAreaCode(String areaCode) {
+            this.areaCode = areaCode;
+        }
+
+        public String getAreaCode() {
+            return areaCode;
+        }
+
+        public void setTelephoneNumber(String number) {
+            this.number = number;
+        }
+
+        public String getTelephoneNumber() {
+            return "(" + areaCode + ") " + number;
+        }
+    }
+
     public class Person {
         private String name;
-        private String officeAreaCode;
-        private String officeNumber;
+        private TelephoneNumber officeTelephone = new TelephoneNumber("02", "1234-5678");
 
         public String getName() {
             return name;
         }
 
         public void setOfficeAreaCode(String officeAreaCode) {
-            this.officeAreaCode = officeAreaCode;
+            officeTelephone.setAreaCode(officeAreaCode);
         }
 
         public String getOfficeAreaCode() {
-            return officeAreaCode;
+            return officeTelephone.getAreaCode();
         }
 
         public void setOfficeNumber(String officeNumber) {
-            this.officeNumber = officeNumber;
+            officeTelephone.setTelephoneNumber(officeNumber);
         }
 
         public String getOfficeNumber() {
-            return officeNumber;
+            return officeTelephone.getTelephoneNumber();
         }
     }
 }
