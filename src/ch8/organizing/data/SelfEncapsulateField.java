18c53be613fa63f05572e8dabb6509f342302baa
Hoonjin(Desktop) <bwjhj1030@gmail.com>
2015-08-19 23:20:03 +0900

    필드 자체 캡슐화에서 setter, getter를 만들고 모두 간접 접근하도록 함.

--- src/ch8/organizing/data/SelfEncapsulateField.java
+++ src/ch8/organizing/data/SelfEncapsulateField.java
@@ -9,15 +9,31 @@ class Range {
     private int high;
 
     public Range(int low, int high) {
-        this.low = low;
-        this.high = high;
+        setLow(low);
+        setHigh(high);
     }
 
     public boolean includes(int arg) {
-        return arg >= low && arg <= high;
+        return arg >= getLow() && arg <= getHigh();
     }
 
     public void grow(int factor) {
-        high *= factor;
+        setHigh(getHigh() * factor);
+    }
+
+    public void setLow(int low) {
+        this.low = low;
+    }
+
+    public int getLow() {
+        return low;
+    }
+
+    public void setHigh(int high) {
+        this.high = high;
+    }
+
+    public int getHigh() {
+        return high;
     }
 }
