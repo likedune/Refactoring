c9a9b9cb790476924219cdf72fe47b4da3e71995
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-08-20 13:46:55 +0900

    클래스의 단방향연결을 양방향으로 전환에서 Customer클래스에 Set 필드를 추가하고 그것을 사용하도록 Order 클래스를 수정

--- src/ch8/organizing/data/ChangeUnidirectionalAssociationToBidirectional.java
+++ src/ch8/organizing/data/ChangeUnidirectionalAssociationToBidirectional.java
@@ -1,8 +1,21 @@
 package ch8.organizing.data;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ChangeUnidirectionalAssociationToBidirectional {
 
     public class Customer {
+
+        private Set orders;
+
+        public Customer() {
+            orders = new HashSet();
+        }
+
+        public Set friendOrders() {
+            return orders;
+        }
     }
 
     public class Order {
@@ -10,7 +23,13 @@ public class ChangeUnidirectionalAssociationToBidirectional {
         Customer customer;
 
         public void setCustomer(Customer customer) {
+            if (this.customer != null) {
+                this.customer.friendOrders().remove(this);
+            }
             this.customer = customer;
+            if (this.customer != null) {
+                this.customer.friendOrders().add(this);
+            }
         }
 
         public Customer getCustomer() {
