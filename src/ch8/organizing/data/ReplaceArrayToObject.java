1aaf34a39657e4ce14faf2150d986ecd5eb73b77
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-08-20 13:12:03 +0900

    배열을 객체로 전환에서 배열의 정보를 가질 클래스를 선언하고 setter, getter를 만든다

--- src/ch8/organizing/data/ReplaceArrayToObject.java
+++ src/ch8/organizing/data/ReplaceArrayToObject.java
@@ -3,11 +3,35 @@ package ch8.organizing.data;
 public class ReplaceArrayToObject {
 
     public static void main(String[] args) {
-        String[] row = new String[3];
-        row[0] = "리버풀";
-        row[1] = "15";
+        Performance performance = new Performance();
+        performance.setName("리버풀");
+        performance.setWins("15");
 
-        String name = row[0];
-        int wins = Integer.parseInt(row[1]);
+        String name = performance.getName();
+        int wins = performance.getWins();
+    }
+}
+
+class Performance {
+    private String[] row;
+
+    public Performance() {
+        row = new String[3];
+    }
+
+    public void setName(String name) {
+        row[0] = name;
+    }
+
+    public String getName() {
+        return row[0];
+    }
+
+    public void setWins(String wins) {
+        row[1] = wins;
+    }
+
+    public int getWins() {
+        return Integer.parseInt(row[1]);
     }
 }
