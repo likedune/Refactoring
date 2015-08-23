e343ce1fe114b5d020375e0adc0d3bef94c84617
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-08-23 15:06:50 +0900

    분류부호를 클래스로 전환에서 앞으로 더이상 사용하지 않을 Person 클래스의 혈액형 변수들을 지워버린다

--- src/ch8/organizing/data/replaceTypeCodeWithClass/Person.java
+++ src/ch8/organizing/data/replaceTypeCodeWithClass/Person.java
@@ -2,11 +2,6 @@ package ch8.organizing.data.replaceTypeCodeWithClass;
 
 public class Person {
 
-    public static final int O = BloodGroup.O.getCode();
-    public static final int A = BloodGroup.A.getCode();
-    public static final int B = BloodGroup.B.getCode();
-    public static final int AB = BloodGroup.AB.getCode();
-
     private BloodGroup bloodGroup;
 
     public Person(BloodGroup bloodGroup) {
@@ -16,7 +11,7 @@ public class Person {
     public void setBloodGroup(BloodGroup bloodGroup) {
         this.bloodGroup = bloodGroup;
     }
-    
+
     public int getBloodGroupCode() {
         return bloodGroup.getCode();
     }
