5919e0a50b449ee9c67724d712e24c4f04782cc5
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-08-23 14:48:14 +0900

    Replace Type Code With Class 분류부호를 클래스로 전환 init

---
+++ src/ch8/organizing/data/replaceTypeCodeWithClass/Person.java
@@ -0,0 +1,23 @@
+package ch8.organizing.data.replaceTypeCodeWithClass;
+
+public class Person {
+
+    public static final int O = 0;
+    public static final int A = 1;
+    public static final int B = 2;
+    public static final int AB = 3;
+
+    private int bloodGroup;
+
+    public Person(int bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public void setBloodGroup(int bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public int getBloodGroup() {
+        return bloodGroup;
+    }
+}
