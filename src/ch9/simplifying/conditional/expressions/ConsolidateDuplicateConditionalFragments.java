dfec419724b97845a498d4d7aed83852e28c330e
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-08-23 14:35:01 +0900

    Consolidate Duplicate Conditional Fragments 조건문의 공통실행코드 빼내기 init

---
+++ src/ch9/simplifying/conditional/expressions/ConsolidateDuplicateConditionalFragments.java
@@ -0,0 +1,26 @@
+package ch9.simplifying.conditional.expressions;
+
+public class ConsolidateDuplicateConditionalFragments {
+
+    private double total;
+    private double price;
+
+    public void priceSender() {
+        if (isSpecialDeal()) {
+            total = price * 0.95;
+            send();
+        }
+        else {
+            total = price * 1.0;
+            send();
+        }
+    }
+
+    private boolean isSpecialDeal() {
+        return true;
+    }
+
+    private void send() {
+        System.out.println("send!");
+    }
+}
