01744bba5c1a2d6828c9341b42c7f00b629bc429
Hoonjin(Desktop) <bwjhj1030@gmail.com>
2015-08-30 22:43:54 +0900

    상태변경 메서드와 값반환 메서드 분리 다시 init

--- src/ch10/making/method/calls/simpler/SeparateQueryFromModifier.java
+++ src/ch10/making/method/calls/simpler/SeparateQueryFromModifier.java
@@ -1,13 +1,19 @@
 package ch10.making.method.calls.simpler;
 
 public class SeparateQueryFromModifier {
+
+    private void sendAlert() {
+        System.out.println("find!");
+    }
     
     private String foundMiscreant(String[] people) {
         for (int i = 0; i < people.length; i++) {
             if (people[i].equals("Don")) {
+                sendAlert();
                 return "Don";
             }
             if (people[i].equals("John")) {
+                sendAlert();
                 return "John";
             }
         }
