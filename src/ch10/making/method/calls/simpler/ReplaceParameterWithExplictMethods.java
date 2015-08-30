0873a79d1b72e85f4be9017752fc9a3edb5d77fc
Hoonjin(Desktop) <bwjhj1030@gmail.com>
2015-08-30 23:08:30 +0900

    매개변수를 메서드로 전환에서 분기를 각기 다른 메서드로 만들고 기존의  create 메서드를 삭제한다

--- src/ch10/making/method/calls/simpler/ReplaceParameterWithExplictMethods.java
+++ src/ch10/making/method/calls/simpler/ReplaceParameterWithExplictMethods.java
@@ -3,21 +3,17 @@ package ch10.making.method.calls.simpler;
 public class ReplaceParameterWithExplictMethods {
 
     public class Employee {
-        private static final int ENGINEER = 0;
-        private static final int SALESMAN = 1;
-        private static final int MANAGER = 2;
+        
+        public Employee createEngineer() {
+            return new Engineer();
+        }
 
-        public Employee create(int type) {
-            // 원래는 static method. 현재는 편의상 이런 방식의 코드를 사용
-            switch (type) {
-                case ENGINEER:
-                    return new Engineer();
-                case SALESMAN:
-                    return new Salesman();
-                case MANAGER:
-                    return new Manager();
-            }
-            return null;
+        public Employee createSalesman() {
+            return new Salesman();
+        }
+
+        public Employee createManager() {
+            return new Manager();
         }
     }
 
